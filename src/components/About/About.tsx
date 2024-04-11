9fe7c153002581781c4c6d1ec8c1ef176eb2beae
aSykoutris <134492329+aSykoutris@users.noreply.github.com>
2024-04-11 12:58:34 +0300

    .

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -37,45 +37,6 @@ export default function About({}: Props) {
       className='mx-auto flex w-full max-w-4xl flex-row items-start justify-center space-x-10 '
       ref={ref}
     >
-      {/* <svg
-        width='100%'
-        height='100%'
-        viewBox='0 0 538 915'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-        className='flex-shrink-0'
-      >
-        <path d={PATH} stroke='url(#paint0_linear_207_38)' />
-        <defs>
-          <linearGradient
-            id='paint0_linear_207_38'
-            x1='0'
-            y1='0'
-            x2='1'
-            y2='0'
-            gradientUnits='userSpaceOnUse'
-          >
-            <stop stop-color='#3879E7' stop-opacity='0' />
-            <stop offset='1' stop-color='#3879E7' />
-          </linearGradient>
-        </defs>
-        <motion.path
-          // animatng pathLength value, goes from 1 to 0
-          style={{
-            pathLength: useSpring(pathLengthValue, {
-              stiffness: 500,
-              damping: 100
-            })
-          }}
-          transition={transition}
-          d={PATH}
-          stroke='var(--blue-500)'
-          strokeOpacity='1'
-          strokeLinecap={'round'}
-          strokeWidth='3'
-        />
-      </svg> */}
-
       <motion.svg
         width='100%'
         height='100%'
@@ -108,8 +69,8 @@ export default function About({}: Props) {
           transition={transition}
           d={LEFT_PATH}
           stroke='blue'
-          stroke-width='10'
-          stroke-linecap='round'
+          strokeWidth='10'
+          strokeLinecap='round'
         />
 
         <path d={RIGHT_PATH} stroke='url(#paint0_linear_207_38)' />
@@ -137,8 +98,8 @@ export default function About({}: Props) {
           transition={transition}
           d={RIGHT_PATH}
           stroke='red'
-          stroke-width='10'
-          stroke-linecap='round'
+          strokeWidth='10'
+          strokeLinecap='round'
         />
       </motion.svg>
 
