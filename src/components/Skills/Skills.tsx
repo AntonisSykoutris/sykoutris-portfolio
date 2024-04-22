fa443ef251fe3ed11284ce0e19a27893dc6c302e
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-04-22 14:10:59 +0300

    .

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,8 +7,13 @@ type Props = {};
 
 export default function Skills({}: Props) {
   return (
-    <Section leftTopCross rightTopCross className='relative z-10'>
-      <h1 className='text-4xl  text-black'>SKILSSSSSSSSSSS</h1>
+    <Section
+      leftTopCross
+      rightTopCross
+      className='bg-dot-black/[0.2] relative z-10 '
+    >
+      <div className='pointer-events-none absolute inset-0 z-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
+      <h1 className='z-50 text-4xl'>aaaaaaaaaaa</h1>
     </Section>
   );
 }
