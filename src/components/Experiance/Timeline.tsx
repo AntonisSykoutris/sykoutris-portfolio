8367e8e9b5533f0a862e1d5ee0c0d575dc1527a7
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-07-26 14:50:14 +0300

    timeline

--- src/components/Experiance/Timeline.tsx
+++ src/components/Experiance/Timeline.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
 
 import React from 'react';
@@ -7,35 +8,48 @@ import {
 } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { experiencesData } from '@/lib/data';
+import { useInView } from 'react-intersection-observer';
 
 export default function Timeline() {
+  // const { ref } = useSectionInView('Experience');
+
   return (
-    <VerticalTimeline lineColor='#e5e7eb'>
-      {experiencesData.map((item, index) => (
-        <React.Fragment key={index}>
-          <VerticalTimelineElement
-            visible
-            contentStyle={{
-              background: '#f3f4f6',
-              boxShadow: 'none',
-              border: '1px solid rgba(0, 0, 0, 0.05)',
-              textAlign: 'left',
-              padding: '1.3rem 2rem'
-            }}
-            contentArrowStyle={{
-              borderRight: '0.4rem solid #9ca3af'
-            }}
-            date={item.date}
-            icon={<item.icon />}
-          >
-            <h3 className='font-semibold capitalize'>{item.title}</h3>
-            <p className='!mt-0 font-normal'>{item.location}</p>
-            <p className='!mt-1 !font-normal text-gray-700'>
-              {item.description}
-            </p>
-          </VerticalTimelineElement>
-        </React.Fragment>
-      ))}
+    <VerticalTimeline lineColor='#f3f4f6'>
+      {experiencesData.map((item, index) => {
+        const { ref, inView } = useInView({
+          triggerOnce: true
+        });
+        // const { ref, inView } = useInView({ threshold: 0 }); // Trigger every time
+        return (
+          <div key={index} ref={ref} className='vertical-timeline-element'>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: '#f3f4f6',
+                boxShadow: 'none',
+                border: '1px solid rgba(0, 0, 0, 0.05)',
+                textAlign: 'left',
+                padding: '1.3rem 2rem'
+              }}
+              contentArrowStyle={{
+                borderRight: '0.4rem solid #9ca3af'
+              }}
+              visible={inView}
+              date={item.date}
+              icon={<item.icon />}
+              iconStyle={{
+                background: 'white',
+                fontSize: '1.5rem'
+              }}
+            >
+              <h3 className='font-semibold capitalize'>{item.title}</h3>
+              <p className='!mt-0 font-normal'>{item.location}</p>
+              <p className='!mt-1 !font-normal text-gray-700'>
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </div>
+        );
+      })}
     </VerticalTimeline>
   );
 }
