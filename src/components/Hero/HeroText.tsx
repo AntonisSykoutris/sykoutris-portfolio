9c1830dcce8061b75abf83c4b178599592c775e9
aSykoutris <134492329+aSykoutris@users.noreply.github.com>
2024-07-31 15:43:39 +0300

    Bug fixes

--- src/components/Hero/HeroText.tsx
+++ src/components/Hero/HeroText.tsx
@@ -13,9 +13,9 @@ const HeroText = () => {
 
   return (
     <div className='noselect container absolute z-10 mt-10 flex h-fit flex-col items-center justify-center text-primary-foreground'>
-      <motion.div className='flex cursor-default flex-col items-center justify-center text-center text-xl opacity-100 sm:text-3xl md:text-4xl 2xl:text-6xl 3xl:text-7xl'>
-        <motion.span {...slideInFromLeft()}>Hi, I&#39;m </motion.span>
-        <motion.div className='text-primary' {...slideInFromRight()}>
+      <div className='flex cursor-default flex-col items-center justify-center text-center text-xl opacity-100 sm:text-3xl md:text-4xl 2xl:text-6xl 3xl:text-7xl'>
+        <span>Hi, I&#39;m </span>
+        <div className='text-primary'>
           {name.split('').map((char, index) => {
             if (char === ' ') {
               return ' ';
@@ -31,16 +31,13 @@ const HeroText = () => {
             );
           })}{' '}
           👋
-        </motion.div>
-      </motion.div>
-      <motion.div
-        className='flex justify-between gap-5 pt-10'
-        {...slideInFromBottom()}
-      >
+        </div>
+      </div>
+      <div className='flex justify-between gap-5 pt-10'>
         <motion.a
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className='rounded-lg bg-background px-4 py-2 text-lg font-bold text-foreground md:text-xl'
+          className='rounded-lg bg-background px-4 py-2 text-base font-bold text-foreground sm:text-lg md:text-xl'
           style={{
             transform: 'translateZ(0)',
             backfaceVisibility: 'hidden',
@@ -54,12 +51,12 @@ const HeroText = () => {
           Download CV
         </motion.a>
         <PulsatingButton
-          className='bg-[#399a67] text-lg font-bold md:text-xl'
+          className='bg-[#399a67] text-base font-bold sm:text-lg md:text-xl'
           pulseColor='#0fba74'
         >
           Contact Me
         </PulsatingButton>
-      </motion.div>
+      </div>
     </div>
   );
 };
