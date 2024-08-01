3ced565527486c765e47e8a96aba16d874e89d62
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-08-01 22:28:43 +0300

    finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,10 @@ export const metadata: Metadata = {
     description:
       "Welcome to the portfolio of Sykoutris Antonis, a versatile full-stack developer with a keen eye for front-end design and innovation. Explore my projects, showcasing expertise in creating dynamic, user-friendly web applications that seamlessly blend aesthetics with functionality. Discover the fusion of technology and creativity in every project. Let's build something remarkable together!",
     siteName: 'Sykoutris Antonis Portfolio',
-    url: 'https://sykoutris-wines.vercel.app',
+    url: 'https://sykoutris-portfolio-eight.vercel.app/',
     images: [
       {
-        url: 'https://sykoutris-wines.vercel.app/opengraph-image.png',
+        url: 'https://sykoutris-portfolio-eight.vercel.app/social.png',
         width: 1200,
         height: 630
       }
